361a1a13501327b56007d07d7baa5535e15d51f7
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2026-04-08 21:17:36 +0700

    handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,3 +3,13 @@
 //first part select an element.We can read textContent on that element
 console.log(document.querySelector('.message').textContent);
 
+document.querySelector('.message').textContent = 'You got it 😍!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value=23;
+console.log(document.querySelector('.guess').value);
+
