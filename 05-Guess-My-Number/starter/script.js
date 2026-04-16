7feb4f5306ee922845aa6c1d494f5b31b74e558e
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2026-04-16 23:17:42 +0700

    Fix:Delete old code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,19 +1,6 @@
 'use strict';
 
 //first part select an element.We can read textContent on that element
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'You got it 😍!';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value=23;
-console.log(document.querySelector('.guess').value);
-*/
 
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
