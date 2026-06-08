82336a5264f64ab86d2116023d976f0125ee6fea
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2026-06-08 23:41:27 +0700

    Fix:change little UI the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,7 +5,7 @@
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +18,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //when guess correct
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'You got it 😍!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
